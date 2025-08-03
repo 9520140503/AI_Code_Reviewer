6fc53dae9d50c671a3d4a9ffe55fe79c4333956f
Vishesh Raj <visheshrajput47@gmail.com>
2025-08-03 12:56:20 +0530

    changing frontend url in cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -14,7 +14,7 @@ app.use(cookieParser())
 
 app.use(cors(
     {
-    origin:'http://localhost:5173',
+    origin:'https://codify11.netlify.app/',
     methods: ["GET","POST","PUT"],
     credentials:true
 }
